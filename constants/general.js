c8ceaf3f0a4a20cd22b64bbb8c3dd551e759c349
Igor <igor.zgavec@viberate.com>
2021-06-12 18:47:02 +0200

    ...fixes

--- constants/general.js
+++ constants/general.js
@@ -1,3 +1,4 @@
 export const redditUrls = ["https://old.reddit.com/r/", "https://www.reddit.com/r/"];
 export const redditBaseUrlKey = "url_overridden_by_dest";
 export const tempFilePath = "tempFiles";
+export const videoQualities = ["240", "360", "480", "720", "1080"];
