fb9595e720d29926259f9fe1ce8689dd42e401a5
Igor <igorzgavec3@gmail.com>
2021-06-20 19:28:04 +0200

    added gfycat and imgur to array of base url domains

--- src/constants/general.js
+++ src/constants/general.js
@@ -1,6 +1,6 @@
-const redditUrls = ["https://old.reddit.com/r/", "https://www.reddit.com/r/"];
+const redditUrls = ["https://old.reddit.com/r/", "https://www.reddit.com/r/", ""];
 const redditBaseUrlKey = "url_overridden_by_dest";
-const replyWithBaseUrlDomains = ["https://streamable.com"];
+const replyWithBaseUrlDomains = ["https://streamable.com", "https://gfycat.com/", "https://imgur.com"];
 const tempFilePath = "tempFiles";
 const videoQualities = ["240", "360", "480", "720", "1080"];
 
